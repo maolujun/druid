4c6f0065214dbf745c9aa59e2aba27f8bbe80d09
luchunfen <351@op110.com>
2017-09-30 10:29:40 +0800

    update StatViewServlet.java

--- druid-spring-boot-starter/src/main/java/com/alibaba/druid/support/http/StatViewServlet.java
+++ druid-spring-boot-starter/src/main/java/com/alibaba/druid/support/http/StatViewServlet.java
@@ -1,10 +1,10 @@
 package com.alibaba.druid.support.http;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.function.BiConsumer;
 
 import javax.management.MBeanServerConnection;
 import javax.management.ObjectName;
@@ -19,7 +19,6 @@ import com.alibaba.druid.stat.DruidStatService;
 import com.alibaba.druid.support.logging.Log;
 import com.alibaba.druid.support.logging.LogFactory;
 import com.alibaba.fastjson.JSONObject;
-import com.sun.org.apache.xalan.internal.utils.XMLSecurityManager.NameMap;
 
 public class StatViewServlet extends ResourceServlet{
 	
@@ -39,6 +38,7 @@ public class StatViewServlet extends ResourceServlet{
     public static final String    PARAM_NAME_JMX_URLS      = "jmxUrls";
     public static final String    PARAM_NAME_JMX_USERNAMES = "jmxUsernames";
     public static final String    PARAM_NAME_JMX_PASSWORDS = "jmxPasswords";
+    public static final String    PARAM_NAME_ONE_JMX_KEY= "oneJmxKey";
 
     private DruidStatService      statService             = DruidStatService.getInstance();
 
@@ -73,38 +73,32 @@ public class StatViewServlet extends ResourceServlet{
         //获取jmx的连接信息(支持配置多个连接)
         String jmxUrls = readInitParam(PARAM_NAME_JMX_URLS);
         urlMap = JSONObject.parseObject(jmxUrls,Map.class);
+        String userNames = readInitParam(PARAM_NAME_JMX_USERNAMES);
+    	nameMap = StringUtils.isEmpty(userNames) ? null : JSONObject.parseObject( userNames,Map.class);
+    	String passwords = readInitParam(PARAM_NAME_JMX_PASSWORDS);
+    	passwordMap = StringUtils.isEmpty(userNames) ? null : JSONObject.parseObject( passwords,Map.class);
+        
+    	String paramJmxUrl = readInitParam(PARAM_NAME_JMX_URL);
+    	String paramJmxUsername = readInitParam(PARAM_NAME_JMX_USERNAME);
+    	String paramJmxPassword = readInitParam(PARAM_NAME_JMX_PASSWORD);
+        //把配置的单个连接放进MAp中
+    	if(paramJmxUrl != null) {
+    		urlMap.put(PARAM_NAME_ONE_JMX_KEY, paramJmxUrl);
+    		nameMap.put(PARAM_NAME_ONE_JMX_KEY, paramJmxUsername);
+    		passwordMap.put(PARAM_NAME_ONE_JMX_KEY, paramJmxPassword);
+    	}
+        
         if(urlMap != null) {
-        	String userNames = readInitParam(PARAM_NAME_JMX_USERNAMES);
-        	nameMap = StringUtils.isEmpty(userNames) ? null : JSONObject.parseObject( userNames,Map.class);
-        	String passwords = readInitParam(PARAM_NAME_JMX_PASSWORDS);
-        	passwordMap = StringUtils.isEmpty(userNames) ? null : JSONObject.parseObject( passwords,Map.class);
-        	
-        	if(nameMap != null) {
-        		urlMap.forEach((key,value) ->{
-        			jmxUrl = value;
-                    jmxUsername = nameMap.get(key);
-                    jmxPassword = passwordMap.get(key);
-                    try {
-                        initJmxConn();
-                    } catch (IOException e) {
-                        LOG.error("init jmxs connection error", e);
-                    }
-        		});
-        	}
-        	
-        }else {
-        	// 获取jmx的连接配置信息
-            String param = readInitParam(PARAM_NAME_JMX_URL);
-             if (param != null) {
-                 jmxUrl = param;
-                 jmxUsername = readInitParam(PARAM_NAME_JMX_USERNAME);
-                 jmxPassword = readInitParam(PARAM_NAME_JMX_PASSWORD);
-                 try {
-                     initJmxConn();
-                 } catch (IOException e) {
-                     LOG.error("init jmx connection error", e);
-                 }
-             }
+    		urlMap.forEach((key,value) ->{
+    			jmxUrl = value;
+                jmxUsername = nameMap.get(key);
+                jmxPassword = passwordMap.get(key);
+                try {
+                    initJmxConn();
+                } catch (IOException e) {
+                    LOG.error("init jmxs connection error", e);
+                }
+    		});
         }
     }
     
@@ -164,7 +158,7 @@ public class StatViewServlet extends ResourceServlet{
     	jmxUrl = urlMap.get(key);
     	jmxUsername = nameMap.get(key);
     	jmxPassword = passwordMap.get(key);
-    	if(StringUtils.isEmpty(jmxUrl)) {
+    	if(jmxUrl != null) {
     		JMXServiceURL url = new JMXServiceURL(jmxUrl);
             Map<String, String[]> env = null;
             if (jmxUsername != null) {
@@ -191,14 +185,11 @@ public class StatViewServlet extends ResourceServlet{
      */
     private String getJmxResult(MBeanServerConnection connetion, String url) throws Exception {
         ObjectName name = new ObjectName(DruidStatService.MBEAN_NAME);
-        
         String result = (String) conn.invoke(name, "service", new String[] { url }, new String[] { String.class.getName() });
         return result;
     }
     
-    StringBuilder sb = null;
     String errorResp = null;
-    int num = 0;
     /**
      * 程序首先判断是否存在jmx连接地址，如果不存在，则直接调用本地的duird服务； 如果存在，则调用远程jmx服务。在进行jmx通信，首先判断一下jmx连接是否已经建立成功，如果已经
      * 建立成功，则直接进行通信，如果之前没有成功建立，则会尝试重新建立一遍。.
@@ -207,17 +198,16 @@ public class StatViewServlet extends ResourceServlet{
      * @return 调用服务后返回的json字符串
      */
     protected synchronized String process(String url) {
-    	
-    		String resp = null;
-        	sb = new StringBuilder();
-            if (jmxUrl == null && urlMap == null) {
-                resp = statService.service(url);
-            } else if( urlMap != null){
-            	num  =0;
-            	connMap.forEach((key,value)->{
-            		String respStr = null;
-            		conn = value;
-            		if (value == null) {// 连接在初始化时创建失败
+    	String resp = null;
+    	if(jmxUrl == null && urlMap == null) {
+    		resp = statService.service(url);
+    	}else if(urlMap != null) {
+    		List<String> list = new ArrayList<String>();
+    		urlMap.forEach((key,jmxurl)->{ 
+    			String respStr = null;
+    			if(jmxurl != null) {
+    				conn = connMap != null ? connMap.get(jmxurl) :null;
+    				if (conn == null) {// 连接在初始化时创建失败
                         try {// 尝试重新连接
                             initJmxConn(key);
                         } catch (IOException e) {
@@ -238,67 +228,53 @@ public class StatViewServlet extends ResourceServlet{
                         	respStr = getJmxResult(conn, url);
 //                        	LOG.info("jmxUrl:"+ key + ";respStr:"+respStr);
                         } catch (Exception e) {
+                        	if(respStr == null) {
+                        		if(connMap == null) connMap = new HashMap<String ,MBeanServerConnection>();
+                        		connMap.put(jmxurl, null);
+                        	}
                             LOG.error("get jmx data error", e);
                             errorResp = DruidStatService.returnJSONResult(DruidStatService.RESULT_CODE_ERROR, "get data error" + e.getMessage());
                         }
                     }
-            		JSONObject object = JSONObject.parseObject(respStr);
-        			int resultCode = object.getIntValue("ResultCode");
-            		if( resultCode == 1) {
-            			String content = object.getString("Content");
-//            			LOG.info("respStr:"+respStr);
-            			if(content != null && content.length() > 0) {
-            				if(content.startsWith("[") && content.endsWith("]")) {
-            					sb.append(content.substring(1, content.length()-1)).append(",");
-            				}else {
-            					sb.append(content).append(",");
-            				}
-            				
-            			}
-            			num++;
-            		}
-            	});
-            	
-            	if(!StringUtils.isEmpty(sb.toString())) {
-            		if(num > 1) {
-            			resp = "{\"ResultCode\":1,\"Content\":["+ sb.toString().substring(0,sb.toString().length()-1) +"]}";
-                		LOG.info("returnString:"+resp);
-            		}else {
-            			resp = "{\"ResultCode\":1,\"Content\":"+ sb.toString().substring(0,sb.toString().length()-1) +"}";
-                		LOG.info("returnString:"+resp);
-            		}
-            		
-            	}
-            	
-            	if(StringUtils.isEmpty(sb.toString())) {
-            		resp = errorResp;
-            	}
-            }else {
-            	conn = connMap.get(jmxUrl);
-    			if (conn == null) {// 连接在初始化时创建失败
-    				try {// 尝试重新连接
-    					initJmxConn();
-    				} catch (IOException e) {
-    					LOG.error("init jmx connection error", e);
-    					resp = DruidStatService.returnJSONResult(DruidStatService.RESULT_CODE_ERROR, "init jmx connection error" + e.getMessage());
-    				}
-    				if (conn != null) {// 连接成功
-    					try {
-    						resp = getJmxResult(conn, url);
-    					} catch (Exception e) {
-    						LOG.error("get jmx data error", e);
-    						resp = DruidStatService.returnJSONResult(DruidStatService.RESULT_CODE_ERROR, "get data error:" + e.getMessage());
-    					}
-    				}
-    			} else {// 连接成功
-    				try {
-    					resp = getJmxResult(conn, url);
-    				} catch (Exception e) {
-    					LOG.error("get jmx data error", e);
-    					resp = DruidStatService.returnJSONResult(DruidStatService.RESULT_CODE_ERROR, "get data error" + e.getMessage());
+    				
+    				if(respStr != null) {
+	            		if(url.startsWith("/sql.json")) {
+	                		Map<String , Object> map = JSONObject.parseObject(respStr, Map.class);
+	                		if(map.get("Content") != null) {
+	                			List sublist = (List) map.get("Content");
+	                    		list.addAll(sublist);
+	                		}
+	            		}else if(url.startsWith("/sql-")){
+	            			JSONObject object = JSONObject.parseObject(respStr);
+	            			String content = object.getString("Content");
+	            			if(content != null) list.add(content);
+	            		}else {
+	            			JSONObject object = JSONObject.parseObject(respStr);
+	            			String content = object.getString("Content");
+	            			list.add(content);
+	            		}
     				}
     			}
-            }
-            return resp;
+    		});
+    		
+    		if(list.size() >0) {
+    			if(url.startsWith("/sql.json")) {
+        			resp = "{\"ResultCode\":1,\"Content\":"+ list.toString() +"}";
+            		LOG.info("returnString:"+resp);
+        		}else if(url.startsWith("/sql-")){
+        			resp = "{\"ResultCode\":1,\"Content\":"+ list.get(0) +"}";
+            		LOG.info("returnString:"+resp);
+        		}else {
+        			resp = "{\"ResultCode\":1,\"Content\":"+ list.get(0) +"}";
+            		LOG.info("returnString:"+resp);
+        		}	
+    		}
+    			
+        	if(StringUtils.isEmpty(list.isEmpty())) {
+        		resp = errorResp;
+        	}
+    	}
+    	return resp;
     }
+    
 }
